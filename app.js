0b18e4562656d8d0fa4677e3617337bbdb90ad23
siddharthsingh22 <siddharthpratapsingh21@gmail.com>
2020-04-17 20:12:27 +0530

    many files changed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-app.use(express.static("public"));
-app.listen(process.env.PORT || 3000, function () {
+
+app.listen(3000, function () {
   console.log("Server has started");
 });
 app.set("view engine", "ejs");
