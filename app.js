0fc1be2d7eea34ba14e771e7d2e6811632bcc5dd
siddharthsingh22 <siddharthpratapsingh21@gmail.com>
2020-04-17 19:01:20 +0530

    Changes made to app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var request = require("request");
 app.use(express.static("public"));
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server has started");
 });
 app.set("view engine", "ejs");
