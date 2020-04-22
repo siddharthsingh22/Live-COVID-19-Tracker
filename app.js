0f13f45a6009b2f820436df435d671791e041cca
siddharthsingh22 <siddharthpratapsingh21@gmail.com>
2020-04-22 21:10:22 +0530

    Intial commit for the districtSearch branch Statewise end point in different from the master branch

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server has started");
@@ -8,25 +11,43 @@ app.listen(process.env.PORT || 3000, function () {
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  request("https://covid-india-cases.herokuapp.com/states/", function (
+  request("https://api.covid19india.org/data.json", function (
     error,
     response,
     body
   ) {
     let data = JSON.parse(body);
-    res.render("info", { data: data });
+    res.render("countryInfo", { data: data });
   });
 });
 
 app.get("/stateSearch", function (req, res) {
   var stateName = req.query.stateName;
-
   request("https://covid-india-cases.herokuapp.com/statetimeline/", function (
     error,
     response,
     body
   ) {
-    var stateData = JSON.parse(body);
+    let stateData = JSON.parse(body);
     res.render("stateInfo", { stateData: stateData, stateName: stateName });
+  });
+});
+
+app.post("/stateSearch/districtSearch", function (req, res) {
+  console.log(req.body);
+  var districtName = req.body.districtName;
+
+  request("https://api.covid19india.org/v2/state_district_wise.json", function (
+    error,
+    respose,
+    body
+  ) {
+    var districtWiseData = JSON.parse(body);
+
+    res.render("districtInfo", {
+      stateName: "Kerala",
+      districtWiseData: districtWiseData,
+      districtName: districtName,
+    });
   });
 });
